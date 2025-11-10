08a52c7f290673e9b3db994b672fe9987f726f9d
Perarasu Vishnu J <11249a285@kanchiuniv.ac.in>
2025-11-10 18:54:13 +0530

    Implement MathOperations namespace with functions
    Added a new namespace 'MathOperations' with functions for addition and multiplication, and updated main to demonstrate its usage.

--- 22. User-Defined Namespace.cpp
+++ 22. User-Defined Namespace.cpp
@@ -1,10 +1,46 @@
+Aim:
+To write a C++ program to demonstrate the use of a user-defined namespace.
+
+Algorithm:
+
+1.Start the program.
+2.Define a namespace using the namespace keyword.
+3.Declare variables or functions inside the namespace.
+4.Access the members of the namespace using the scope resolution operator (::).
+5.Display the output.
+6.End the program.
+
+Program:
+
 #include <iostream>
-namespace CSE {
-    void show() {
-        std::cout << "Hello from CSE namespace!" << std::endl;
+using namespace std;
+namespace MathOperations
+{
+    int add(int a, int b)
+    {
+        return a + b;
+    }
+
+    int multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    void displayMessage()
+    {
+        cout << "Inside MathOperations Namespace" << endl;
     }
 }
-int main() {
-    CSE::show();
+
+int main()
+{
+    MathOperations::displayMessage();
+
+    int x = 10, y = 5;
+
+    cout << "Addition = " << MathOperations::add(x, y) << endl;
+    cout << "Multiplication = " << MathOperations::multiply(x, y) << endl;
+
     return 0;
 }
+
