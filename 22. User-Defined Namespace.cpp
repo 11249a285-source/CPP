8c3041bec2e4e62dfef912daed43325e3681b0cf
Perarasu Vishnu J <11249a285@kanchiuniv.ac.in>
2025-11-16 11:50:42 +0530

    Update 22. User-Defined Namespace.cpp

--- 22. User-Defined Namespace.cpp
+++ 22. User-Defined Namespace.cpp
@@ -44,3 +44,10 @@ int main()
     return 0;
 }
 
+Output:
+Inside MathOperations Namespace
+Addition = 15
+Multiplication = 50
+
+Result: The program successfully demonstrates the use of a namespace to group related functions. It shows how to access functions inside a namespace using the scope resolution operator ::.
+
